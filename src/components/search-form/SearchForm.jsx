e31b9b0326cccdbf0ec567a3dd2656b0715934db
Richa299 <richa.singhnxt213@gmail.com>
2023-01-12 18:05:06 +0530

    search form created

--- src/components/search-form/SearchForm.jsx
+++ src/components/search-form/SearchForm.jsx
@@ -1,16 +1,23 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import { useGlobalContext } from "../../context"
 import './Search.css'
 function SearchForm(){
     const {setSearchTerm}=useGlobalContext()
     const searchValue=useRef('');
     
+    useEffect(()=>{
+    searchValue.current.focus();
+    },[])
+
     const searchCocktail=()=>{
         setSearchTerm(searchValue.current.value);
     }
+    const handleSubmit=(e)=>{
+     e.preventDefault()
+    }
 return(
    <div className="form">
-       <form className="search-form">
+       <form className="search-form" onSubmit={handleSubmit}>
         <label htmlFor="name">Search your cocktail</label>
         <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
        </form>
