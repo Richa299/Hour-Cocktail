6bd72c93cc3899c5a29c93c65158f39080c63584
Richa299 <richa.singhnxt213@gmail.com>
2023-01-12 18:08:44 +0530

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import logo from '../../../src/logo.jpg';
-import './Navbar.css'
+import './Navbar.css';
+import { Link } from "react-router-dom"
 function Navbar(){
 return(
     <nav className="navbar">
         <div>
             <img src={logo} className="logo"/>
+            <ul>
+            <Link to="/">details</Link>
+            </ul>
         </div>
     </nav>
 )
